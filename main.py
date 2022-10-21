bfb0ed4b66c4efeb548386d40b96a32561b5c741
Dylan Tettemer <tettemerd@gmail.com>
2022-10-21 06:50:15 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import selenium;
+from selenium import webdriver as wb
+import pandas as pd
+import numpy as np
+import datetime
+import time
+from datetime import date
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
