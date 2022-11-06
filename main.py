a43ee664253b3d51a1afc36a58d53c2c2c823ed3
Dylan Tettemer <tettemerd@gmail.com>
2022-11-06 08:07:37 -0800

    Finished scrape sequence and added final loop

--- main.py
+++ main.py
@@ -11,14 +11,7 @@ import locale
 #Setting up the initital driver along with kayak url then going to the Kayak website
 locale.setlocale( locale.LC_ALL, 'en_US.UTF-8' ) 
 driver = wb.Firefox(executable_path="/Users/dylantettemer/Downloads/geckodriver.exe")
-kayak = "https://www.kayak.com/flights/LAS-PMO/2023-02-06-flexible-3days/2023-02-15-flexible-3days?sort=bestflight_a"
-driver.get(kayak)
 sleep(2)
-
-# Click on the cheapest price button on the site
-cheap_results = '//a[@data-code = "price"]'
-driver.find_element(By.XPATH, cheap_results).click()
-sleep(30)
 
 # Function to load more flight options on the site
 def load_more():
@@ -104,11 +97,140 @@ def page_scrape():
         hours_list.append(schedule.text.split('\n')[0])
         carrier_list.append(schedule.text.split('\n')[1])
         
-    print(carrier_list)
+    a_hours = hours_list[::2]
+    a_carrier = carrier_list[::2]
+    b_hours = hours_list[1::2]
+    b_carrier = carrier_list[1::2]
     
-page_scrape()
+    ### Creating the names of the columns for the dataframe storing all of the flights ###
+    cols = (['Out Day', 'Out Time', 'Out Weekday', 'Out Airline', 'Out Cities', 'Out Duration', 'Out Stops', 'Out Stop Cities',
+            'Return Day', 'Return Time', 'Return Weekday', 'Return Airline', 'Return Cities', 'Return Duration', 'Return Stops', 'Return Stop Cities',
+            'Price'])
+    
+    flights_df = pd.DataFrame({'Out Day': a_day,
+                               'Out Weekday': a_weekday,
+                               'Out Duration': a_duration,
+                               'Out Cities': a_section_names,
+                               'Return Day': b_day,
+                               'Return Weekday': b_weekday,
+                               'Return Duration': b_duration,
+                               'Return Cities': b_section_names,
+                               'Out Stops': a_stop_list,
+                               'Out Stop Cities': a_stops_list,
+                               'Return Stops': b_stop_list,
+                               'Return Stop Cities': b_stop_list,
+                               'Out Time': a_hours,
+                               'Out Airline': a_carrier,
+                               'Return Time': b_hours,
+                               'Return Airline': b_carrier,                           
+                               'Price': prices_list})[cols]
+    
+    flights_df['timestamp'] = strftime("%Y%m%d-%H%M")
+    return flights_df
 
+def start_kayak(city_from, city_to, date_start, date_end):
+   
+    kayak = ('https://www.kayak.com/flights/' + city_from + '-' + city_to +
+             '/' + date_start + '-flexible-3days/' + date_end + '-flexible-3days?sort=bestflight_a')
+    driver.get(kayak)
+    sleep(randint(8,10))
+   
+    try:
+        xp_pop_up_close = '//button[contains(@class="B5la-button") and contains(@class,"Button-No-Standard-Style close ")]'
+        driver.find_elements(By.XPATH, xp_pop_up_close)[5].click()
+        
+    except Exception as e:
+        pass
+    
+    sleep(randint(60, 95))
+    print("Loading more...")
+    
+    load_more()
+    print("Starting first scrape...")
+    
+    df_flights_best = page_scrape()
+    df_flights_best['sort'] = 'best'
+    sleep(randint(60, 80))
+    
+    matrix = driver.find_elements(By.XPATH, '//*[contains(@id,"FlexMatriceCell")]')
+    matrix_prices = [price.text.replace('$', '') for price in matrix]
+    matrix_prices = list(map(int, matrix_prices))
+    matrix_min = min(matrix_prices)
+    matrix_avg = sum(matrix_prices)/len(matrix_prices)
+    
+    print('switching to cheapest results...')
+    cheap_results = '//a[@data-code = "price"]'
+    driver.find_element(By.XPATH, cheap_results).click()
+    sleep(randint(60,90))
+    print('Loading more....')
+    
+    load_more()
+    
+    print('Starting second scrape...')
+    df_flights_cheap = page_scrape()
+    df_flights_cheap['sort'] = 'cheap'
+    sleep(randint(60,80))
+    
+    print("Switching to quickest results...")
+    quick_results = '//a[@data-code = "duration"]'
+    driver.find_element(By.XPATH, quick_results).click()  
+    sleep(randint(60,90))
+    print('loading more.....')
+    
+    load_more()
+    
+    print('Starting third scrape...')
+    df_flights_fast = page_scrape()
+    df_flights_fast['sort'] = 'fast'
+    sleep(randint(60,80))
+    
+    # saving a new dataframe as an excel file. the name is custom made to your cities and dates
+    final_df = df_flights_cheap.append(df_flights_best).append(df_flights_fast)
+    final_df.to_excel('search_backups//{}_flights_{}-{}_from_{}_to_{}.xlsx'.format(strftime("%Y%m%d-%H%M"),
+                                                                                   city_from, city_to, 
+                                                                                   date_start, date_end), index=False)
+    print('saved df.....')
+    
+    # We can keep track of what they predict and how it actually turns out!
+    xp_loading = '//div[contains(@id,"advice")]'
+    loading = driver.find_element(By.XPATH, xp_loading).text
+    xp_prediction = '//span[@class="info-text"]'
+    prediction = driver.find_element(By.XPATH, xp_prediction).text
+    print(loading+'\n'+prediction)
+    
+    weird = '¯\\_(ツ)_/¯'
+    if loading == weird:
+        loading = 'Not sure'
+    
+    username = 'YOUREMAIL@hotmail.com'
+    password = 'Gracie4261996'
+    
+    server = smtplib.SMTP('smtp.outlook.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    msg = ('Subject: Flight Scraper\n\n\
+            Cheapest Flight: {}\nAverage Price: {}\n\nRecommendation: {}\n\nEnd of message'.format(matrix_min, matrix_avg, (loading+'\n'+prediction)))
+    message = MIMEMultipart()
+    message['From'] = 'tettemerd@gmail.com'
+    message['to'] = 'tettemerd@gmail.com'
+    server.sendmail('tettemerd@gmail.com', 'tettemerd@gmail.com', msg)
+    print('sent email.....')
 
+if __name__ == "__main__":
+    
+    city_from = input("Where are you traveling from? Please input IATA Code:  ")
+    city_to = input("Where to? IATA code please:  ")
+    date_start = input("Around which departure date? Please input in format YYYY-MM-DD:  ")
+    date_end = input("Return when? Format YYYY-MM-DD:  ")
+    
+    for n in range(0,5):
+        start_kayak(city_from, city_to, date_start, date_end)
+        
+        print('iteration {} was complete @ {}'.format(n, strftime("%Y%m%d-%H%M")))
+        
+        sleep(60*60*4)
+        print("sleep finished...")
 
 
     
